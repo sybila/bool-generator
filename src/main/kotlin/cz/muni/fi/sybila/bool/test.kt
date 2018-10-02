b0b4b6bb880d3a59745ce5c4a3cec83316e0249a
Matej Hajnal <matej.hajnal@gmail.com>
2018-10-02 15:28:20 +0200

    current state prop failed

--- src/main/kotlin/cz/muni/fi/sybila/bool/test.kt
+++ src/main/kotlin/cz/muni/fi/sybila/bool/test.kt
@@ -26,18 +26,24 @@ fun main(args: Array<String>) {
             ))
     )
 
+    var xy = True
+    var xz = True
+    var yz = True
+    var sx = True
+    var sy = True
+
     val parser = BooleanModelParser()
-    val model2 = parser.readString("""
-        a:1 <- b:1
-        a:2 <- b:1&c
-        b:2 <- a:2|!c
-        b:1 <- a:2&c
-        c <- c&a:2
+    var model2 = parser.readString("""
+        sx <- sx
+        sy <- sy
+        x <- sx
+        y <- x&sy
+        z <- x&y
     """.trimIndent())
     println("Parsed model: $model2")
 
-    println("======= Run Model 1 =======")
-    runExperiments(model1)
+    // println("======= Run Model 1 =======")
+    // runExperiments(model1)
     println("======= Run Model 2 =======")
     runExperiments(model2)
 }
@@ -45,19 +51,73 @@ fun main(args: Array<String>) {
 fun runExperiments(model: BooleanModel) = BooleanFragment(model).run {
             /*val aGreaterOne = "a".asVariable() ge 1.0.asConstant()
             val states = aGreaterOne.eval()*/
-
-            println("Verify EF(a = 0 & b = 0 & c = 0)")
             val zero = 0.0.asConstant()
-            val allZero = ("a".asVariable() eq zero) and ("b".asVariable() eq zero) and ("c".asVariable() eq zero)
+            val one = 1.0.asConstant()
+            val allZero = ("x".asVariable() eq zero) and ("y".asVariable() eq zero) and ("z".asVariable() eq zero)
+
+            println("Verify EF(x = 0 & y = 0 & z = 0)")
             val reachZero = EF(allZero)
             SequentialChecker(this).use { checker ->
                 val states = checker.verify(reachZero)
                 for ((s, _) in states.entries()) {
                     println(s.prettyPrint(model))
                 }
+                println(states.sizeHint)
             }
-            println("Reachability for a=0, b=2, c=1")
-            val init = intArrayOf(0, 2, 1)
+
+            println("Verify delay_on: AU((z = 0),(y = 1))")
+            val delay_on = (("sx".asVariable() eq one) and ("sy".asVariable() eq one) and ("x".asVariable() eq zero) and ("y".asVariable() eq zero) and ("z".asVariable() eq zero) and ("z".asVariable() eq zero)AU("y".asVariable() eq one))
+            SequentialChecker(this).use { checker ->
+                val states = checker.verify(delay_on)
+                for ((s, _) in states.entries()) {
+                    println(s.prettyPrint(model))
+                }
+                println(states.sizeHint)
+            }
+
+            println("Verify delay_on1: AU((z = 0),(y = 1))")
+            val delay_on1 = (("z".asVariable() eq zero)AU("y".asVariable() eq one))
+            SequentialChecker(this).use { checker ->
+                val states = checker.verify(delay_on1)
+                for ((s, _) in states.entries()) {
+                    println(s.prettyPrint(model))
+                }
+                println(states.sizeHint)
+            }
+
+            println("Verify no_delay_off AF(~(S(z=1,y=1,sx=0,sy=1) & EX(S(y=0))) | EX(S(y=0,z=0)))")
+            val no_delay_off = AF(not( ("z".asVariable() eq one) and ("y".asVariable() eq one) and ("sx".asVariable() eq zero) and ("sy".asVariable() eq one)) and EX("y".asVariable() eq zero)) or EX(("y".asVariable() eq zero) and ("z".asVariable() eq zero))
+            SequentialChecker(this).use { checker ->
+                val states = checker.verify(no_delay_off)
+                for ((s, _) in states.entries()) {
+                    println(s.prettyPrint(model))
+                }
+                println(states.sizeHint)
+            }
+
+            println("Verify delay_off: AU((z = 1),(y = 0))")
+            val delay_off = ("z".asVariable() eq one)AU("y".asVariable() eq zero)
+            SequentialChecker(this).use { checker ->
+                val states = checker.verify(delay_off)
+                for ((s, _) in states.entries()) {
+                    println(s.prettyPrint(model))
+                }
+                println(states.sizeHint)
+            }
+
+
+            println("Verify no_delay_on AF(~(S(z=0,y=0,sx=1,sy=1) & EX(S(y=1))) | EX(S(y=1,z=1)))")
+            val no_delay_on = AF(not( ("z".asVariable() eq zero) and ("y".asVariable() eq zero) and ("sx".asVariable() eq one) and ("sy".asVariable() eq one)) and EX("y".asVariable() eq one)) or EX(("y".asVariable() eq one) and ("z".asVariable() eq one))
+            SequentialChecker(this).use { checker ->
+                val states = checker.verify(no_delay_on)
+                for ((s, _) in states.entries()) {
+                    println(s.prettyPrint(model))
+                }
+                println(states.sizeHint)
+            }
+
+            println("Reachability for sx=1, sy=1, x=0, y=1, z=1")
+            val init = intArrayOf(1, 1, 0, 1, 1)
             var recompute = setOf<Int>(model.encoder.encodeState(init))
             var discovered = recompute
             while (recompute.isNotEmpty()) {
@@ -67,5 +127,7 @@ fun runExperiments(model: BooleanModel) = BooleanFragment(model).run {
                 recompute = step - discovered
                 discovered += recompute
             }
+
+
         }
 
