c66b3259b6b62858fd715d85b988907da9419b67
Samuel Pastva <daemontus@gmail.com>
2019-03-12 09:58:00 +0100

    proof of concept

--- src/main/kotlin/cz/muni/fi/sybila/bool/rg/p53.kt
+++ src/main/kotlin/cz/muni/fi/sybila/bool/rg/p53.kt
@@ -9,7 +9,7 @@ data class State(
         val Mdm2nuc: Int
 ) {
 
-    override fun toString(): String = "[$p53, $Mdm2cyt, $DNAdam, $Mdm2nuc]"
+    override fun toString(): String = "[$p53, $DNAdam, $Mdm2cyt, $Mdm2nuc]"
 }
 
 /**
@@ -43,7 +43,7 @@ data class Graph(
         return result
     }
 
-    private fun Set<Int>.next(): Set<Int> = this.flatMap {
+    fun Set<Int>.next(): Set<Int> = this.flatMap {
         successors[it] ?: emptyList()
     }.toSet()
 
@@ -83,7 +83,7 @@ fun main() {
         (0..2).flatMap { Mdm2cyt ->
             (0..1).flatMap { DNAdam ->
                 (0..1).map {  Mdm2nuc ->
-                    State(p53, Mdm2cyt, DNAdam, Mdm2nuc)
+                    State(p53 = p53, Mdm2cyt = Mdm2cyt, DNAdam =  DNAdam, Mdm2nuc = Mdm2nuc)
                 }
             }
         }
@@ -99,7 +99,7 @@ fun main() {
         val observable = p[0] != p[1]
         val inhibition = p[1] <= p[0]
         observable && inhibition
-    }.toList()
+    }.toList().map { P53UpdateFunction(it) }
 
     // domain [0,1], one [0,1] regulation, one [0,1,2] regulation - size of table = 6
     val DNAParametrisations = buildParametrisations(1, 6).filter { p ->
@@ -117,30 +117,143 @@ fun main() {
         val DNAobservability = p[0] != p[3] || p[1] != p[4] || p[2] != p[5]
         val DNAactivation = p[3] >= p[0] && p[4] >= p[1] && p[5] >= p[2]
         p53Interval && p53observability && p53inhibition && DNAobservability && DNAactivation
-    }.toList()
+    }.toList().map { DNAdamUpdateFunction(it) }
+
+    // domain [0,1,2], one [0,1,2] regulation - size of table = 3
+    val Mdm2cytParametrisations = buildParametrisations(2, 3).filter { p ->
+        // Logical table
+        // P53
+        // 0    p0
+        // 1    p1
+        // 2    p2
+        val p53Interval = p[0] == p[1]
+        val p53observability = p[0] != p[2]
+        val p53activation = p[2] >= p[0]
+        p53Interval && p53observability && p53activation
+    }.toList().map { Mdm2cytUpdateFunction(it) }
+
+    // domain [0,1], two [0,1,2] regulations, one [0,1] regulation - size of table = 18
+    val Mdm2nucParametrisations = buildParametrisations(1, 18).filter { p ->
+        // Logical table
+        // Mdm2cyt  p53 DNA
+        //[1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1]
+        // 0        0   0   p0
+        // 1        0   0   p1
+        // 2        0   0   p2
+        // 0        1   0   p3
+        // 1        1   0   p4
+        // 2        1   0   p5
+        // 0        2   0   p6
+        // 1        2   0   p7
+        // 2        2   0   p8
+        // 0        0   1   p9
+        // 1        0   1   p10
+        // 2        0   1   p11
+        // 0        1   1   p12
+        // 1        1   1   p13
+        // 2        1   1   p14
+        // 0        2   1   p15
+        // 1        2   1   p16
+        // 2        2   1   p17
+        // 1,2 are equivalent
+        val p53Interval = p[3] == p[6] && p[4] == p[7] && p[5] == p[8] && p[12] == p[15] && p[13] == p[16] && p[14] == p[17]
+        val p53Observability = p[0] != p[3] || p[1] != p[4] || p[2] != p[5] || p[9] != p[12] || p[10] != p[13] || p[11] != p[14]
+        val p53inhibition = p[0] >= p[3] && p[1] >= p[4] && p[2] >= p[5] && p[9] >= p[12] && p[10] >= p[13] && p[11] >= p[14]
+        val DNAObservability = p[0] != p[9] || p[1] != p[10] || p[2] != p[11] || p[3] != p[12] || p[4] != p[13] || p[5] != p[14] || p[6] != p[15] || p[7] != p[16] || p[8] != p[17]
+        val DNAInhibition = p[0] >= p[9] && p[1] >= p[10] && p[2] >= p[11] && p[3] >= p[12] && p[4] >= p[13] && p[5] >= p[14] && p[6] >= p[15] && p[7] >= p[16] && p[8] >= p[17]
+        val M2CObservability1 = p[0] != p[1] || p[3] != p[4] || p[6] != p[7] || p[9] != p[10] || p[12] != p[13] || p[15] != p[16]
+        val M2CObservability2 = p[2] != p[1] || p[5] != p[4] || p[8] != p[7] || p[11] != p[10] || p[14] != p[13] || p[17] != p[16]
+        val M2CActivation = p[1] >= p[0] && p[2] >= p[1] && p[4] >= p[3] && p[5] >= p[4] && p[7] >= p[6] && p[8] >= p[7] && p[10] >= p[9] && p[11] >= p[10] && p[13] >= p[12] && p[14] >= p[13] && p[16] >= p[15] && p[17] >= p[16]
+        p53Interval && p53Observability && p53inhibition && DNAObservability && DNAInhibition && M2CObservability1 && M2CObservability2 && M2CActivation
+    }.toList().map { Mdm2nucUpdateFunction(it) }
 
     println("p53 num parametrisations: ${p53Parametrisations.size}")
     println("DNA num parametrisations: ${DNAParametrisations.size}")
-/*
+    println("Mdm2cyt num parametrisations: ${Mdm2cytParametrisations.size}")
+    println("Mdm2nuc num parametrisations: ${Mdm2nucParametrisations.size}")
+
     val indices = stateSpace.mapIndexed { index, state -> state to index }.toMap()
 
-    fun Int.transition(update: State.() -> State): Pair<Int, Int> {
+    fun Int.transition(update: UpdateFunction): Pair<Int, Int> {
         val from = stateSpace[this]
-        val to = from.update()
+        val to = update.transform(from)
         return this to indices.getValue(to)
     }
 
-    val edges = mutableListOf<Pair<Int, Int>>()
+    val fenotypes = mutableSetOf<Set<SCC>>()
+
+    /*p53Parametrisations.forEach { p53 ->
+        DNAParametrisations.forEach { DNA ->
+            Mdm2cytParametrisations.forEach { Mdm2cyt ->
+                //Mdm2nucParametrisations.forEach { Mdm2nuc ->
+                val Mdm2nuc = Mdm2nucUpdateFunction(listOf(1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1))
+                    val edges = mutableListOf<Pair<Int, Int>>()
+                    for (i in stateSpace.indices) {
+                        edges += i.transition(p53)
+                        edges += i.transition(DNA)
+                        edges += i.transition(Mdm2cyt)
+                        edges += i.transition(Mdm2nuc)
+                    }
+
+                    val graph = Graph(stateSpace.size, edges.toSet())
+
+                    val tscc = graph.tscc(stateSpace.indices.toSet())
+                    val sizes = tscc.map { it.size }
+                    fenotypes.add(tscc)
+                    println(sizes)
+                    println(tscc.map { graph.probablyCycle(it) })
+                    /*if (sizes == listOf(1)) {
+                        println("Size: $sizes")
+                        println("p53: ${p53.parametrisation}")
+                        println("DNA: ${DNA.parametrisation}")
+                        println("Mdm2cyt: ${Mdm2cyt.parametrisation}")
+                        println("Mdm2nuc: ${Mdm2nuc.parametrisation}")
+                        /*val scc = tscc.find { it.size == 27 }!!
+                        println(scc.map { stateSpace[it] })
+                        scc.forEach {
+                            println("${stateSpace[it]} -> ${graph.run { setOf(it).next() }.map { stateSpace[it] }}")
+                        }*/
+                    }*/
+                //}
+            }
+        }
+    }*/
+
+    val p53 = P53UpdateFunction(listOf(2,1))
+    val DNA = DNAdamUpdateFunction(listOf(1,1,0,1,1,1))
+    val Mdm2cyt = Mdm2cytUpdateFunction(listOf(1, 1, 2))
+    val Mdm2nuc = Mdm2nucUpdateFunction(listOf(1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1))
+
+    val edges = mutableSetOf<Pair<Int, Int>>()
     for (i in stateSpace.indices) {
-        edges += i.transition(State::updatep53)
-        edges += i.transition(State::updateMdm2cyt)
-        edges += i.transition(State::updateMdm2nuc)
-        edges += i.transition(State::updateDNAdam)
+        edges += i.transition(p53)
+        edges += i.transition(DNA)
+        edges += i.transition(Mdm2cyt)
+        edges += i.transition(Mdm2nuc)
     }
 
-    val graph = Graph(stateSpace.size, edges.toSet())
+    /*edges.forEach {
+        println("${stateSpace[it.first]} -> ${stateSpace[it.second]}")
+    }*/
 
-    println(graph.tscc(stateSpace.indices.toSet()).map { scc -> scc.map { stateSpace[it] } })*/
+    val graph = Graph(stateSpace.size, edges)
+
+    val tscc = graph.tscc(stateSpace.indices.toSet())
+
+    val sink = State(p53 = 1, DNAdam = 0, Mdm2nuc = 0, Mdm2cyt = 0)
+    println(DNA.run { sink.index() })
+    println(DNA.invoke(sink))
+    println(tscc.map { scc -> scc.map { stateSpace[it] } })
+
+    fenotypes.forEach {
+        println(it.map { scc -> scc.map { stateSpace[it] } })
+    }
+}
+
+fun Graph.probablyCycle(scc: SCC): Boolean {
+    return scc.all { s ->
+        (setOf(s).next() - s).size <= 1
+    }
 }
 
 fun Int.update(target: Int): Int = when {
@@ -200,26 +313,31 @@ fun buildParametrisations(max: Int, numParams: Int): Sequence<Parametrisation> =
  * 2,1,2 p17
  */
 abstract class UpdateFunction(
-        private val parametrisation: Parametrisation
+        val parametrisation: Parametrisation
 ) : (State) -> TargetValue {
     override fun invoke(p1: State): TargetValue = parametrisation[p1.index()]
     abstract fun State.index(): Int
+    abstract fun transform(state: State): State
 }
 
 // implementations of update functions based on the regulatory network
 
 class Mdm2nucUpdateFunction(parametrisation: Parametrisation) : UpdateFunction(parametrisation) {
     override fun State.index(): Int = Mdm2cyt + (3 * p53) + (9 * DNAdam)
+    override fun transform(state: State): State = state.copy(Mdm2nuc = state.Mdm2nuc.update(invoke(state)))
 }
 
 class DNAdamUpdateFunction(parametrisation: Parametrisation) : UpdateFunction(parametrisation) {
-    override fun State.index(): Int = DNAdam + (2 * p53)
+    override fun State.index(): Int = p53 + (3 * DNAdam)
+    override fun transform(state: State): State = state.copy(DNAdam = state.DNAdam.update(invoke(state)))
 }
 
 class P53UpdateFunction(parametrisation: Parametrisation) : UpdateFunction(parametrisation) {
     override fun State.index(): Int = Mdm2nuc
+    override fun transform(state: State): State = state.copy(p53 = state.p53.update(invoke(state)))
 }
 
 class Mdm2cytUpdateFunction(parametrisation: Parametrisation) : UpdateFunction(parametrisation) {
     override fun State.index(): Int = p53
+    override fun transform(state: State): State = state.copy(Mdm2cyt = state.Mdm2cyt.update(invoke(state)))
 }
