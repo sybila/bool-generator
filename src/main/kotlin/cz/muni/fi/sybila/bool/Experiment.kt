5e2565b1ebd072ed5d7252fdec7b71e18b4174e5
Samuel Pastva <daemontus@gmail.com>
2019-05-19 10:18:01 +0200

    simple model example

---
+++ src/main/kotlin/cz/muni/fi/sybila/bool/Experiment.kt
@@ -0,0 +1,54 @@
+package cz.muni.fi.sybila.bool
+
+import cz.muni.fi.sybila.bool.rg.network
+import cz.muni.fi.sybila.bool.solver.BDDSolver
+
+fun main() {
+    // #P = 48, |P| = 4.67e5, |S| = 32
+    val G2A = network {
+        val CtrA = specie("CtrA")
+        val SciP = specie("SciP")
+        val CcrM = specie("CcrM")
+        val DnaA = specie("DnaA")
+        val GcrA = specie("GcrA")
+
+        GcrA activates CtrA
+        CtrA activates CtrA
+        SciP inhibits CtrA
+        CcrM inhibits CtrA
+
+        CtrA activates SciP
+        DnaA inhibits SciP
+
+        CtrA activates CcrM
+        SciP inhibits CcrM
+        CcrM inhibits CcrM
+
+        GcrA inhibits DnaA
+        CtrA activates DnaA
+        DnaA inhibits DnaA
+        CcrM activates DnaA
+
+        CtrA inhibits GcrA
+        DnaA activates GcrA
+/*
+        GcrA given listOf(DnaA)
+
+        CtrA given listOf(CtrA)
+        CtrA given listOf(CtrA, GcrA)
+        CtrA given listOf(GcrA)
+
+        SciP given listOf(CtrA)
+
+        CcrM given listOf(CtrA)
+
+        DnaA given listOf(CtrA, CcrM)
+ */
+    }
+
+    val model = G2A.asBooleanModel()
+    val solver = BDDSolver(model.parameterCount)
+    val admissibleParameters = G2A.staticConstraintsBDD(solver)
+
+    
+}
